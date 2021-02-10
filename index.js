4fe259309bb5fda1626721bd7a50d1e64fb0236d
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-02-10 12:16:17 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+var path = require('path');
+
+
+function makeid(length, characterSet) {
+    var result           = '';
+    var characters       = characterSet;
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res)=>{ 
+    res.sendFile(path.join(__dirname + '/index.html'));
+    }); 
+
+// simple route
+app.get("/api/randpass", (req, res) => {
+    var length = req.query.length
+    var charset = req.query.charset
+    var password = makeid(length, charset);
+    res.json([
+        {
+            password: password 
+        }
+    ]);
+});
+
+// set port, listen for requests
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.");
+});
